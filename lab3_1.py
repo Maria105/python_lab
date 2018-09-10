8682e81679d7d0d5aef93b18dd0f6ec1bbd9cfc8
Maria105 <36701641+Maria105@users.noreply.github.com>
2018-09-10 12:54:45 +0300

    Add files via upload

---
+++ lab3_1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import math
+
+a=int(input('введіть перше число '))
+b=int(input('введіть друге число '))
+
+print('сума', a+b )
---
+++ lab3_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#-*- coding: utf -8 -*-
+
+import math
+
+Far=input('Введіть температуру у Фаренгейт: ')
+
+Cel=(int(Far)-32)*5/7
+print('температура у Цельсіях:' + str(Cel) )
