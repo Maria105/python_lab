88a0f5a103878bfd37317bedef9e6832ba3833f4
Maria105 <36701641+Maria105@users.noreply.github.com>
2018-09-10 12:59:15 +0300

    Delete lab3_2.py

--- lab3_2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#-*- coding: utf -8 -*-
-
-import math
-
-Far=input('Введіть температуру у Фаренгейт: ')
-
-Cel=(int(Far)-32)*5/7
-print('температура у Цельсіях:' + str(Cel) )
