92bcca2bce59a9a3a0f892db3ede3a0c06319b93
Maria105 <36701641+Maria105@users.noreply.github.com>
2018-09-10 10:26:20 +0300

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import sys
+arg=sys.argv[1:]
+a=' '.join(arg)
+print('Hello,'+a)
---
+++ lab3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*-coding: utf-8  -*-
+
+
+print('What is your name?')
+a=input()
+print('What is your surname?')
+b=input()
+print ('Hello,'+a + ' ' +b)
+
+
+
