a149f01a7671008934209972be74ef67632d0de8
Maria105 <36701641+Maria105@users.noreply.github.com>
2018-09-10 12:59:24 +0300

    Delete lab3_3.py

--- lab3_3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-#-*- coding: utf-8 -*-
-
-weight=float(input(' введіть власну вагу в кілограмах:'))
-height=float(input('введіть власний зріст в метрах: '))
-
-Body_mass_index=weight/height**2
-print('Ваш індекс маси тіла: ' + str(Body_mass_index) )
