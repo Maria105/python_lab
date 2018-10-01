317a1eb0d2ca3bd0ac66211f6a1749a6ddf91c87
Maria105 <36701641+Maria105@users.noreply.github.com>
2018-10-01 13:15:50 +0300

    Add files via upload

---
+++ lab6_2.py
@@ -0,0 +1,19 @@
+##!/usr/bin/env python3
+
+def input_datas() -> list:
+    """This function takes datas about you deposit"""
+
+    a = int(input('enter your summ: '))
+    b = int(input('enter annual interest rate in percent: '))
+    c = int(input('enter duration of the deposit: '))
+
+    return [a, b, c]
+
+def  (lst: list) -> float:
+    """This function calculation area of triangle"""
+
+
+    s =  a ( pow ( ( 1 + b ), c ))
+    return (s)
+def output_summ(s: float) -> float:
+    print ('summ ')
