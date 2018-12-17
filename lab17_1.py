16abf8c8c82353f67c8b2500b7af9cf9a1dc4e04
Maria105 <36701641+Maria105@users.noreply.github.com>
2018-12-17 11:42:42 +0200

    Add files via upload

---
+++ lab17_1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import unittest
+
+class Test_Calculate(unittest.TestCase):
+    def test_perimetr(self):
+        self.assertEqual(calc.(3,4,5), 6)
+    def test_
+
+if __name__ == '__main__':
+    unittest.main()
